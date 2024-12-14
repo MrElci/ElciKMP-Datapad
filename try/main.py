6546441b3e01df13888c4f28142920d92e3c9ba1
MrElci <77832525+MrElci@users.noreply.github.com>
2024-12-14 22:01:37 +0300

    Re-upload

---
+++ try/main.py
@@ -0,0 +1,14 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
