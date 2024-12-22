16878d3e18650cf86e96880b6ca14efe20b78d3b
MrElci <77832525+MrElci@users.noreply.github.com>
2024-12-22 15:41:38 +0300

    BFPA 0.0.2.0
    - Atil Ved finished
    - Person img class added succ
    - War of latin-turco add succ

--- ElciKMP-WIKI/todo.py
+++ ElciKMP-WIKI/todo.py
@@ -6,5 +6,5 @@
 #olay şablonu yaz
 #Ünvanlar sayfası ekle
 #Sunucu ideolojik ve ekonomik dönem ve çağ listelemesi ekle
-#haritaları temize çek
-#naziarasistan kısmı yazılıcak
+#person.img bitirmek
+#xenia yapılcak
