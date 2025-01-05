9d02add33ce55d1b575eb88e5fc0808d6522d933
MrElci <77832525+MrElci@users.noreply.github.com>
2025-01-05 11:37:54 +0300

    BFPA 0.0.3.6
    - The permission of Lourxz taked
    - Efe cenger started

--- ElciKMP-WIKI/todo.py
+++ ElciKMP-WIKI/todo.py
@@ -1,10 +1,8 @@
 #yapılcaklar
 
-#Kıtaları yaz
+#Efe cengeri bitir, milata kadar dönemi bitir.
 #kayıp çağları yaz
 #çağ sınıflandırmasını ages yazısına ekle
 #olay şablonu yaz
 #Ünvanlar sayfası ekle
 #Sunucu ideolojik ve ekonomik dönem ve çağ listelemesi ekle
-
-#xenia yapılcak
